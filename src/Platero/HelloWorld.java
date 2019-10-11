83f4e33f6b92ea1d6f45c91c3fdc3cf58cb807e8
Jeremias Platero <lightjedi5@byui.edu>
2019-10-11 14:22:24 -0600

    Added HelloWorld class

---
+++ src/Platero/HelloWorld.java
@@ -0,0 +1,11 @@
+package Platero;
+
+public class HelloWorld {
+    static void sayHello(){
+        System.out.println("Hello , my name is <your name>");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
